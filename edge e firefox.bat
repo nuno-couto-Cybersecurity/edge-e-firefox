7b6f6fc98a93df5973fd5f05f28be37ec1144d23
Nuno Couto <nunomiguelnmc@gmail.com>
2026-06-23 16:39:07 +0100

    Refactor edge e firefox.bat to use placeholder URLs
    Removed specific URLs and replaced them with placeholders.

--- edge e firefox.bat
+++ edge e firefox.bat
@@ -1,19 +1,19 @@
 @echo off
-start msedge http://sonhov2.chporto.min-saude.pt:9001/forms/frmservlet?config=sonho_chp
+start msedge http://
 powershell - command "start-sleep -milliseconds 200"
-start msedge http://rnu.min-saude.pt/rnu/idu/pesquisaUtente.do
+start msedge http://
 powershell - command "start-sleep -milliseconds 200"
-start msedge http://p1.min-saude.pt/indexcth.htm
+start msedge http://
 powershell - command "start-sleep -milliseconds 200"
-start msedge https://chp-portint.chporto.min-saude.pt/
+start msedge https://
 powershell - command "start-sleep -milliseconds 200"
 start msedge https://teams.microsoft.com/v2/
 powershell - command "start-sleep -milliseconds 200"
-start msedge https://outlook.office.com/mail/f00007@chporto.min-saude.pt/
+start msedge https://outlook.office.com/mail/
 powershell - command "start-sleep -milliseconds 200"
-start msedge http://sclinicov2.chporto.min-saude.pt:9001/forms/frmservlet?config=sclinico
+start msedge http://
 powershell - command "start-sleep -milliseconds 200"
-start msedge https://ctv.ulssa.min-saude.pt:8090/wapps/#/login
+start msedge https://
 powershell - command "start-sleep -milliseconds 200"
-start msedge https://chp-portint.chporto.min-saude.pt/aida_urgencia/Login.aspx
-start firefox https://chp-portint.chporto.min-saude.pt/portal/e-internamentog/index.php
+start msedge https://
+start firefox https://
